fe6953535972928d1191e45a543d24d252d7e4b4
Adonis142857 <77864027+Adonis142857@users.noreply.github.com>
2023-07-20 11:40:00 +0800

    Update signin.sh

--- signin.sh
+++ signin.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-COOKIE=$1
-RANDOM=$2
+IFS=$'\n'
+COOKIES=($COOKIE)
 
-if [ "$RANDOM" = "true" ]; then
-  URL='https://www.nodeseek.com/api/attendance?random=true'
-else
-  URL='https://www.nodeseek.com/api/attendance?random=false'
-fi
+for COOKIE in ${COOKIES[@]}; do
+  if [ "$Random" = "true" ]; then
+    response=$(curl -s 'https://www.nodeseek.com/api/attendance?random=true' --compressed -X POST \
+      -H "Cookie: $COOKIE" \
+      ...)
 
-response=$(curl -s "$URL" --compressed \
-    -X POST \
-    -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0' \
-    -H 'Accept: */*' \
-    -H 'Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2' \
-    -H 'Accept-Encoding: gzip, deflate, br' \
-    -H 'Referer: https://www.nodeseek.com/board' \
-    -H 'Origin: https://www.nodeseek.com' \
-    -H 'Connection: keep-alive' \
-    -H "Cookie: $COOKIE" \
-    -H 'Sec-Fetch-Dest: empty' \
-    -H 'Sec-Fetch-Mode: cors' \
-    -H 'Sec-Fetch-Site: same-origin' \
-    -H 'Content-Length: 0' \
-    -H 'TE: trailers')
+    success=$(echo $response | jq -r .success)
+    message=$(echo $response | jq -r .message)
+    current=$(echo $response | jq -r .current)
 
-success=$(echo $response | jq -r '.success')
-message=$(echo $response | jq -r '.message')
-current=$(echo $response | jq -r '.current')
+    if [ "$success" = "true" ]; then
+      echo "签到成功，$message，如今有$current个鸡腿"
+    else
+      echo "签到失败，错误信息：$message"
+    fi
+  else
+    response=$(curl 'https://www.nodeseek.com/api/attendance?random=false' --compressed -X POST \
+      -H "Cookie: $COOKIE" \
+      ...)
 
-if [ "$success" = "true" ]; then
-  echo "签到成功，$message，如今有$current个鸡腿"
-else
-  echo "签到失败，错误信息：$message"
-fi
+    success=$(echo $response | jq -r '.success')
+    if [ "$success" = true ] ; then
+      message=$(echo $response | jq -r '.message')
+      gain=$(echo $response | jq -r '.gain')
+      current=$(echo $response | jq -r '.current')
+      echo "签到成功，$message，如今有$current 个鸡腿"
+    else
+      echo "签到失败，错误信息：$message"
+    fi
+  fi
+done
